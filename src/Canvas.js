a6a4171c18bd9ab5ff2037552705e18aa60b881f
katrinagarloff <katrinagarloff@gmail.com>
2018-08-17 13:47:56 -0400

    made helper functions for ball movement

--- src/Canvas.js
+++ src/Canvas.js
@@ -18,7 +18,9 @@ export default class Canvas extends Component {
       down: false,
       left: false,
       right: false
-    }
+    },
+    ballCollision: false,
+    timer: 0
   }
 
   drawBall = (ctx, ballRadius, color, x, y) => {
@@ -29,99 +31,73 @@ export default class Canvas extends Component {
       ctx.closePath()
   }
 
+  switchDirection = (dir) => {
+    this.setState(prevState => {
+      return {
+        bouncingBall: {
+          ...prevState.bouncingBall,
+          [dir]: -prevState.bouncingBall[dir] }
+        }
+      }, () => this.move()
+    )
+  }
+
+  move = () => {
+    this.setState(prevState => {
+      return {
+        bouncingBall: {
+          ...prevState.bouncingBall,
+        x: prevState.bouncingBall.x += this.state.bouncingBall.dirX,
+        y: prevState.bouncingBall.y += this.state.bouncingBall.dirY }
+      }
+    })
+  }
+
   draw = (canvas, ctx) => {
 
-    const ballRadius = 10
+    const ballRadius = 20
 
       ctx.clearRect(0, 0, canvas.width, canvas.height)
 
       this.drawBall(ctx, ballRadius, "#0095DD", this.state.bouncingBall.x, this.state.bouncingBall.y)
 
-      if (this.state.bouncingBall.x + this.state.bouncingBall.dirX > canvas.width-ballRadius || this.state.bouncingBall.x + this.state.bouncingBall.dirX < ballRadius) {
-          this.setState(prevState => {
-            return {
-              bouncingBall: {
-                ...prevState.bouncingBall,
-                dirX: -prevState.bouncingBall.dirX }
-              }
-              },
-            () => {
-              this.setState(prevState => {
-                return {
-                  bouncingBall: {
-                    ...prevState.bouncingBall,
-                  x: prevState.bouncingBall.x += this.state.bouncingBall.dirX,
-                  y: prevState.bouncingBall.y += this.state.bouncingBall.dirY }
-                }
-            })
-            })
+      if (this.state.bouncingBall.x + this.state.bouncingBall.dirX >  canvas.width-ballRadius || this.state.bouncingBall.x + this.state.bouncingBall.dirX < ballRadius) {
+
+        this.switchDirection("dirX")
       }
       if(this.state.bouncingBall.y + this.state.bouncingBall.dirY > canvas.height-ballRadius || this.state.bouncingBall.y + this.state.bouncingBall.dirY < ballRadius) {
-          this.setState(prevState => {
-            return {
-              bouncingBall:
-                { ...prevState.bouncingBall,
-                  dirY: -prevState.bouncingBall.dirY }
-            }
-          }, () => {
-            this.setState(prevState => {
-              return {
-                bouncingBall:
-                { ...prevState.bouncingBall,
-                  x: prevState.bouncingBall.x += this.state.bouncingBall.dirX,
-                  y: prevState.bouncingBall.y += this.state.bouncingBall.dirY }
-              }
-            })
-          })
+
+        this.switchDirection("dirY")
+
         } else {
-          this.setState(prevState => {
-            return {
-              bouncingBall:
-              { ...prevState.bouncingBall,
-                x: prevState.bouncingBall.x += this.state.bouncingBall.dirX,
-                y: prevState.bouncingBall.y += this.state.bouncingBall.dirY }
-            }
-          })
+          this.move()
         }
   }
 
 
+  updateArrow = (dir, bool) => {
+    this.setState(prevState => {
+      return {
+        arrow:
+        { ...prevState.arrow,
+          [dir]: bool }
+      }
+    })
+  }
+
   changeArrowState = (e, bool) => {
+
     if(e.key === "ArrowUp") {
-      this.setState(prevState => {
-        return {
-          arrow:
-          { ...prevState.arrow,
-            up: bool }
-        }
-      })
+      this.updateArrow("up", bool)
 
     } else if (e.key === "ArrowDown") {
-      this.setState(prevState => {
-        return {
-          arrow:
-          { ...prevState.arrow,
-            down: bool }
-        }
-      })
+      this.updateArrow("down", bool)
 
     } else if (e.key === "ArrowLeft") {
-      this.setState(prevState => {
-        return {
-          arrow:
-          { ...prevState.arrow,
-            left: bool }
-        }
-      })
+      this.updateArrow("left", bool)
 
     } else if (e.key === "ArrowRight") {
-      this.setState(prevState => {
-        return {
-          arrow:
-          { ...prevState.arrow,
-            right: bool }
-        }
-      })
+      this.updateArrow("right", bool)
     }
   }
 
@@ -132,37 +108,61 @@ export default class Canvas extends Component {
   handleKeyUp = (e) => {
     this.changeArrowState(e, false)
   }
-  
+
   moveUserBall = (canvas, ctx) => {
     const ballRadius = 10
 
     this.drawBall(ctx, ballRadius, '#ebf442', this.state.userBall.x, this.state.userBall.y)
 
-
-  }
-
-  startingPosition = (x, y) => {
-    this.setState(prevState => {
-      return {
-        userBall:
+    if(this.state.arrow.up && this.state.userBall.y -4 > 0 + ballRadius) {
+      // console.log(`ball y ${this.state.userBall.y} canvas height${canvas.height}`)
+      this.setState(prevState => {
+        return {
+          userBall:
           { ...prevState.userBall,
-            x: x,
-            y: y }
-      }
-    })
+            // x: prevState.userBall.x += 4,
+            y: prevState.userBall.y += -4 }
+        }
+      })
+    } else if (this.state.arrow.down && this.state.userBall.y +4 < canvas.height - ballRadius) {
+      this.setState(prevState => {
+        return {
+          userBall:
+          { ...prevState.userBall,
+            y: prevState.userBall.y += 4 }
+        }
+      })
+    }  else if (this.state.arrow.left && this.state.userBall.x -4 > 0 + ballRadius) {
+      this.setState(prevState => {
+        return {
+          userBall:
+          { ...prevState.userBall,
+            x: prevState.userBall.x += -4 }
+        }
+      })
+    } else if (this.state.arrow.right && this.state.userBall.x +4 < canvas.width - ballRadius) {
+      this.setState(prevState => {
+        return {
+          userBall:
+          { ...prevState.userBall,
+            x: prevState.userBall.x += 4 }
+        }
+      })
+    }
   }
+
 
   componentDidMount(){
     const canvas = document.getElementById("myCanvas")
     const ctx = canvas.getContext("2d")
-    const ballRadius = 10
+
 
     this.setState({
         bouncingBall:
         { x: canvas.width/2,
           y: canvas.height-30,
-          dirX: 2,
-          dirY: -2 },
+          dirX: 3,
+          dirY: -3 },
         userBall:
         { x: canvas.width/3,
           y:canvas.height-20,
@@ -184,8 +184,10 @@ export default class Canvas extends Component {
 
     return (
       <div>
+        { this.state.ballCollision ?
+        <p> you lost </p>
+        : <canvas id="myCanvas" width={800} height={800}  onKeyDown={this.handleKeyDown} onKeyUp={this.handleKeyUp} tabIndex="0"> </canvas> }
 
-        <canvas id="myCanvas" width={800} height={800} onKeyDown={this.handleKeyDown} onKeyUp={this.handleKeyUp} tabIndex="0"> </canvas>
 
 
       </div>
